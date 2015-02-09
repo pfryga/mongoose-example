152ae9ce18a734a092d805b74e653e9df7634a84
Piotr Fryga <piotr.fryga@allegro.pl>
2015-02-09 15:32:37 +0100

    example service endpoints

--- app.js
+++ app.js
@@ -1,8 +1,42 @@
 var express = require('express');
+var mongoose = require('mongoose');
 var app = express();
+
+mongoose.connect('mongodb://localhost/test');
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'db connection error:'));
+db.once('open', function (callback) {
+  console.log('db connection opened!');
+});
+
+var Cat = mongoose.model('Cat', {
+    name: String
+});
 
 app.get('/ping', function(req, res) {
     res.send('pong');
+});
+
+app.post('/insert/:name', function(req, res) {
+    var kitty = new Cat({
+        name: req.params.name
+    });
+
+    kitty.save(function () {
+        console.log('inserted: ' + req.params.name);
+        res.send('inserted: ' + req.params.name);
+    });
+});
+
+app.post('/delete/:name', function(req, res) {
+    Cat.find({
+        name: req.params.name
+    }).remove(function () {
+        console.log('removed: ' + req.params.name);
+        res.send('removed: ' + req.params.name);
+    });
 });
 
 app.listen(3000, function () {
